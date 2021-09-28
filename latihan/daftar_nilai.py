fb75b10d4bf580ff4f01ac8bf0385f6c880814e5
Sirus Laia <laia.sirus@gmail.com>
2021-09-28 21:10:16 +0100

    Update kode aplikasi daftar nilai

--- latihan/daftar_nilai.py
+++ latihan/daftar_nilai.py
@@ -1,4 +1,7 @@
-print('''
+# Ini adalah aplikasi memasukkan nilai ijazah siswa ke dalam database
+#
+# Sekedar tampilan judul
+judul = '''
     \_/
   --(_)--  .
     / \   /_\\
@@ -16,13 +19,22 @@ Aplikasi Nilai Ijazah Siswa
 
 
 '''
-)
+# Sekedar tampilan bak catatan kaki
+catatan_kaki = '''
+Belajar itu seumur hidup
 
-# Mendeklarasikan nama daftar
+       _.-'`'-._
+    .-'    _    '-.
+     `-.__  `\\_.-'
+       |  `-``\\|
+       `-.....-A
+               #
+               #
+'''
 
-daftar_nilai = []
+# Mendefinisikan berbagai fungsi
 
-# Fungsi untuk memasukkan data ke daabase
+# Fungsi untuk memasukkan data ke database
 def simpan_data(nama, gender, nilai):
     daftar_nilai.append({"nama": nama, "gender": gender, "nilai": nilai})
     print("Trims. Data telah ditambahkan!")
@@ -35,8 +47,52 @@ def formulir_memasukkan_data():
 
     simpan_data(nama, gender, nilai)
 
+# Menghitung nilai rata-rata
+def hitung_nilai_rata():
+    total_nilai = 0
+    for i in daftar_nilai:
+        total_nilai += int(i["nilai"])
+    
+    return total_nilai / len(daftar_nilai)
+
+# Mencari siswa dengan nilai tertinggi
+def cari_nilai_tertinggi():
+    nilai = daftar_nilai[0]["nilai"]
+    nama = daftar_nilai[0]["nama"]
+
+    for i in daftar_nilai:
+        nilai_siswa = i["nilai"]
+        if nilai_siswa > nilai:
+            nilai = nilai_siswa
+            nama = i["nama"]
+    return nama, nilai
+
+# Mencari siswa dengan nilai terrendah
+def cari_nilai_terrendah():
+    nilai = daftar_nilai[0]["nilai"]
+    nama = daftar_nilai[0]["nama"]
+
+    for i in daftar_nilai:
+        nilai_siswa = i["nilai"]
+        if nilai_siswa <= nilai:
+            nilai = nilai_siswa
+            nama = i["nama"]
+    return nama, nilai
+
+
+# Program mulai dari sini
+
+# Mendeklarasikan nama daftar
+daftar_nilai = []
+
+# Mencetak judul
+print(judul)
+
+# Menampilkan formulir pemasukkan data
+# Buat variable untuk mengontrol selesai/maju
 terus = True
 
+# Tampilkan formulir dan pesan setelah selesai
 while terus:
     formulir_memasukkan_data()
 
@@ -56,66 +112,29 @@ print()
 print(f"Inilah data yang telah masuk dalam database: \n{daftar_nilai}")
 print()
 
+
 # Menghitung nilai rata-rata
-
-def hitung_nilai_rata():
-    for i in daftar_nilai:
-        total_nilai = 0
-        total_nilai += int(i["nilai"])
-    
-    return total_nilai / len(daftar_nilai)
-
+# Buat variable
 nilai_rata = hitung_nilai_rata()
 
+# Tampilkan di layar nilai rata-rata
 print(f"Nilai rata-rata  : {round(nilai_rata, 2)}")
 
-# Mencari siswa dengan nilai tertinggi
-def cari_nilai_tertinggi():
-    nilai = daftar_nilai[0]["nilai"]
-    nama = daftar_nilai[0]["nama"]
-
-    for i in daftar_nilai:
-        nilai_siswa = i["nilai"]
-        if nilai_siswa > nilai:
-            nilai = nilai_siswa
-            nama = i["nama"]
-    return nama, nilai
-
+# Mencari siswa dengan nilai tertinggi dan tanpilkan di layar
 print(f"Nilai tertinggi  : {cari_nilai_tertinggi()[0]} (nilai: {cari_nilai_tertinggi()[1]})")
 
-# Mencari siswa dengan nilai terrendah
-def cari_nilai_terrendah():
-    nilai = daftar_nilai[0]["nilai"]
-    nama = daftar_nilai[0]["nama"]
-
-    for i in daftar_nilai:
-        nilai_siswa = i["nilai"]
-        if nilai_siswa <= nilai:
-            nilai = nilai_siswa
-            nama = i["nama"]
-    return nama, nilai
-
+# Mencari siswa dengan nilai terrendah dan tampilkan di layar
 print(f"Nilai terrendah  : {cari_nilai_terrendah()[0]} (nilai: {cari_nilai_terrendah()[1]})")
 print("*" * 20)
-print('''
 
-Belajar itu seumur hidup
-
-       _.-'`'-._
-    .-'    _    '-.
-     `-.__  `\\_.-'
-       |  `-``\\|
-       `-.....-A
-               #
-               #
-''')
+# Mencetak catatan kaki
+print(catatan_kaki)
 
 
 
 
 
-
-
+# Sekedar isi database kalau diperlukan
 # daftar_nilai.append({"nama": "Siti", "gender": "perempuan", "nilai": 9})
 # daftar_nilai.append({"nama": "Budi" , "gender": "laki-laki", "nilai": 7})
 # daftar_nilai.append({"nama": "Tuti", "gender": "perempuan", "nilai": 8})
