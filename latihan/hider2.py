a65ec5f8e61ebe3dc0628e1279015d878344ffb4
Sirus Laia <laia.sirus@gmail.com>
2021-10-29 14:38:13 +0100

    Memperbaiki fungsi dan json

--- latihan/hider2.py
+++ latihan/hider2.py
@@ -4,41 +4,83 @@ import json
 def menginput_menu():
     data_makanan = []
     
-    nama_makanan = input("Masukkan nama makanan : ") 
-    harga_makanan = int(input("Masukkan harga        : "))
+    selesai = False
+    
+    while selesai == False:
+        nama_makanan = input("Masukkan nama makanan : ") 
+        harga_makanan = int(input("Masukkan harga        : "))
 
-    data_makanan.append({"nama": nama_makanan, "harga": harga_makanan})
+        data_makanan.append({"nama": nama_makanan, "harga": harga_makanan})
 
-    with open('toko_hider.json', 'w') as json_file:
-        json.dump(data_makanan, json_file)
-        json_file.close()
+        pertanyaan = input("Masih ada lagi? y/t : ")
+
+        if pertanyaan == "y":
+            selesai = False
+        else:
+            selesai = True
+            with open('hider_menu.json', 'w') as json_file:
+            json.dump(data_makanan, json_file)
 
 def menginput_transaksi():
     data_transaksi = []
     
-    tanggal =  input("Masukkan tanggal      : ")
-    item_makanan = input("Masukkan nama makanan : ") 
-    jumlah =  input("Masukkan jumlah       : ")
-    total_harga = int(input("Masukkan harga        : "))
+    selesai = False
+    
+    while selesai == False:
+        tanggal =  input("Masukkan tanggal      : ")
+        item_makanan = input("Masukkan nama makanan : ") 
+        jumlah =  input("Masukkan jumlah       : ")
+        total_harga = int(input("Masukkan harga        : "))
+        
+        data_transaksi.append({tanggal: "", item_makanan: "", jumlah: "", total_harga: ""})
+        
+        pertanyaan = input("Masih ada lagi? y/t : ")
 
-    data_transaksi.append({tanggal: "", item_makanan: "", jumlah: "", total_harga: ""})
-
-    with open('toko_hider.json', 'w') as json_file:
-        data_transaksi = json.dump(json_file)
+        if pertanyaan == "y":
+            selesai = False
+        else:
+            selesai = True
+            # simpan data yang telah ada ke dalam database        
+    	    with open('hider_transaksi.json', 'w') as json_file:
+            json.dump(data_transaksi, json_file)
 
 def menginput_anggota():
     data_anggota = []
     
-    nama = input("Masukkan nama makanan : ") 
-    nomor = input("Masukkan harga        : ")
+    selesai = False
+    
+    while selesai == False:
+        nama = input("Masukkan nama makanan : ") 
+        nomor = input("Masukkan harga        : ")
+        
+        data_anggota.append({nama: "", nomor: ""})
+        
+        pertanyaan = input("Masih ada lagi? y/t : ")
 
-    data_anggota.append({nama: "", nomor: ""})
+        if pertanyaan == "y":
+            selesai = False
+        else:
+            selesai = True
+            # simpan data yang telah ada ke dalam database
+            with open('hider_anggota.json', 'w') as json_file:
+            json.dump(data_anggota, json_file)
+        
+# Mulai dari sini program
 
-    with open('toko_hider.json', 'w') as json_file:
-        data_anggota = json.dump(json_file)
+print("Pilihan aksi")
+print()
+print("1. Menginput transaksi")
+print("2. Menginput anggota")
+print("3. Menginput menu")
+print()
+pilihan = input("Silakan memilih: (1/2/3) ")
 
-
-menginput_menu()
+if pilihan == "3":
+    menginput_menu()
+elif pilihan == "2":
+    menginput_anggota()
+else:
+    menginput_transaksi()
 
 
 
